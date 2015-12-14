b7b9ffb9f463a89ff718267b0b6bfa28dfaee176
Andre Tomt <andre@tomt.net>
2015-12-14 06:18:12 +0100

    Add JSON reading and writing to ACNE::Util::File

--- share/perl/ACNE/Util/File.pm
+++ share/perl/ACNE/Util/File.pm
@@ -5,6 +5,19 @@ use warnings FATAL => 'all';
 use autodie;
 use Carp qw(croak carp);
 
+use JSON;
+
+sub readJSON {
+	my ($path) = @_;
+	open my $fh, '<', $path;
+	do { local $/; decode_json(<$fh>) };
+}
+
+sub writeJSON {
+	my ($data, $path) = @_;
+	open my $fh, '>', $path;
+	print $fh encode_json($data);
+}
 
 # XXX filter comments, blank lines, etc.
 sub readPairs {
