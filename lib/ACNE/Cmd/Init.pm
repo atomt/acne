6265aef3aff1c4255734765a73fed9aedfdcaee7
Andre Tomt <andre@tomt.net>
2015-12-20 21:10:04 +0100

    Clean up acme init a bit, make it set up challenge root

--- lib/ACNE/Cmd/Init.pm
+++ lib/ACNE/Cmd/Init.pm
@@ -11,8 +11,9 @@ use English qw(-no_match_vars);
 sub run {
 	ACNE::Common::config();
 
-	my $store = catdir(@{$config->{'system'}->{'store'}});
-	my $user  = $config->{'system'}->{'user'};
+	my $store    = catdir(@{$config->{'system'}->{'store'}});
+	my $acmeroot = catdir(@{$config->{'challenge'}->{'http01fs'}->{'acmeroot'}});
+	my $user     = $config->{'system'}->{'user'};
 
 	say 'Setting up Acne store to work with current configuration';
 	say '';
@@ -28,14 +29,29 @@ sub run {
 	say "Configured user $user has uid $uid and gid $gid";
 
 	# Set up system.store
-	# Not using perl built-ins is a little silly, but -p and -R is so much easier.
-	say "Creating system.store $store";
-	-e $store ? say "system.store $store already exists." : systemv('mkdir', '-p', '-m', '0750', $store);
-	systemv('chmod', '0750', $store);
-	systemv('chown', '-R', $uid . ':' . $gid, $store);
+	say "Creating system.store in $store";
+	mkdirv($store, '0755');
+	systemv('chmod', '0755', $store);
 
+	for my $p ( qw(account cert live) ) {
+		my $fp = catdir($store, $p);
+		mkdirv(catdir($fp), '0750');
+		systemv('chmod', '0750', $fp);
+	}
+
+	say "Creating challenge.http10fs.acmeroot $acmeroot";
+	mkdirv($acmeroot, '0755');
+	systemv('chmod', '0755', $acmeroot);
+
+	systemv('chown', '-R', $uid . ':' . $gid, $store);
 	say '';
 	say 'Acne should be ready for use.';
+}
+
+# Not using perl built-ins is a little silly, but -p and -R is so much easier.
+sub mkdirv {
+	my ($fp, $perms) = @_;
+	-e $fp ? say "$fp already exists." : systemv('mkdir', '-p', '-m', $perms, $fp);
 }
 
 sub systemv {
