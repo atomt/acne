04fd776a2bf3b2c9ead80df9358eb8d95ad5efbe
Andre Tomt <andre@tomt.net>
2016-03-19 01:23:06 +0100

    Install to sbin, in line with Debian standards

--- debian/acne.cron.d
+++ debian/acne.cron.d
@@ -1,3 +1,3 @@
 MAILTO=root
 
-31 20 * * * root if [ -x /usr/bin/acne ]; then /usr/bin/acne renew-auto --cron; fi
+31 20 * * * root if [ -x /usr/sbin/acne ]; then /usr/sbin/acne renew-auto --cron; fi
